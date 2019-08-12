6ad46fcc372f9b181f1342d126a64c2109ec1f6f
chentong1 <chentong1@staff.weibo.com>
2019-08-12 22:52:46 +0800

    init repo

---
+++ app/src/main/java/com/buaa/ct/lockscreen/LockReceiver.java
@@ -0,0 +1,32 @@
+package com.buaa.ct.lockscreen;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by 10202 on 2016/4/11.
+ */
+public class LockReceiver extends DeviceAdminReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        System.out.println("onreceiver");
+    }
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        System.out.println("激活使用");
+        super.onEnabled(context, intent);
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        System.out.println("取消激活");
+        super.onDisabled(context, intent);
+    }
+
+
+}
